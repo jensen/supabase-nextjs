3d59e071459cbda5bfff3b2afa493ff8862c4a64
Karl Jensen <karljensen@gmail.com>
2022-10-25 11:26:53 -0700

    Switch to Turbopack

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
-import "../styles/globals.css";
+import "../styles/generated.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
