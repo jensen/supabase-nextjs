efebc3c67f4967d60cff84f7a6f6bd5451f685c3
Karl Jensen <karljensen@gmail.com>
2022-11-04 11:44:22 -0700

    Use supabase abstraction over browser client

--- services/supabase/client.ts
+++ services/supabase/client.ts
@@ -1,4 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-shared";
+
 import type { Database } from "./types/db";
 
 export const createSupabaseClient = () => {
@@ -10,35 +11,8 @@ export const createSupabaseClient = () => {
     throw new Error(`Must define process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY`);
   }
 
-  const cookie = JSON.parse(
-    document.cookie
-      .split(";")
-      .map((v) => v.split("="))
-      .reduce((acc, v) => {
-        acc[decodeURIComponent(v[0].trim()) as "supabase-auth-token"] =
-          decodeURIComponent(v[1].trim());
-        return acc;
-      }, {} as { "supabase-auth-token": string })["supabase-auth-token"]
-  );
-
-  const authorization = cookie?.access_token
-    ? {
-        global: {
-          headers: {
-            Authorization: `Bearer ${cookie.access_token}`,
-          },
-        },
-      }
-    : {};
-
-  return createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      auth: {
-        persistSession: false,
-      },
-      ...authorization,
-    }
-  );
+  return createBrowserSupabaseClient<Database>({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  });
 };
